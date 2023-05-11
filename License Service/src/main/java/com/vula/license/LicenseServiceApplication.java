3358f1b3315d5b95d8406514c7e8390668094281
vu-luong <vubinhcht@gmail.com>
2023-05-11 21:45:18 +1000

    use autoconfig message source

--- License Service/src/main/java/com/vula/license/LicenseServiceApplication.java
+++ License Service/src/main/java/com/vula/license/LicenseServiceApplication.java
@@ -3,7 +3,6 @@ package com.vula.license;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -21,13 +20,5 @@ public class LicenseServiceApplication {
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
         localeResolver.setDefaultLocale(Locale.US);
         return localeResolver;
-    }
-
-    @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setUseCodeAsDefaultMessage(true);
-        messageSource.setBasenames("messages");
-        return messageSource;
     }
 }
